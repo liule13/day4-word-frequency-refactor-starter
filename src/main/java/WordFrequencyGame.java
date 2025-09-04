1af42fd2bace5fbfeda53713c9d07fa39cfd6d19
liule13 <LEO.LIU@OOCL.COM>
2025-09-04 15:35:40 +0800

    refactor: reformat the code and remove useless blank line. Rename arr to words. Extract constant ANY_SPACE_SEPARATOR.Extract method countFrequencies. Rename inputList to frequencies.Simplify countFrequencies return statement.Rename list to frequencies in countFrequencies.

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -8,67 +8,61 @@ import java.io.CharArrayWriter;
 import java.time.LocalDateTime;
 
 public class WordFrequencyGame {
-    public String getResult(String inputStr){
 
+    public static final String ANY_SPACE_SEPARATOR = "\\s+";
 
-        if (inputStr.split("\\s+").length==1) {
+    public String getResult(String inputStr) {
+        if (inputStr.split(ANY_SPACE_SEPARATOR).length == 1) {
             return inputStr + " 1";
         } else {
-
             try {
-
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
+                String[] words = inputStr.split(ANY_SPACE_SEPARATOR);
+                List<Input> frequencies = countFrequencies(words);
 
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map =getListMap(inputList);
-
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()){
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
-                }
-                inputList = list;
-
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                frequencies.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : inputList) {
-                    String s = w.getValue() + " " +w.getWordCount();
+                for (Input w : frequencies) {
+                    String s = w.getValue() + " " + w.getWordCount();
                     joiner.add(s);
                 }
                 return joiner.toString();
             } catch (Exception e) {
-
-
                 return "Calculate Error";
             }
         }
     }
 
+    private List<Input> countFrequencies(String[] words) {
+        List<Input> inputList = new ArrayList<>();
+        for (String s : words) {
+            Input input = new Input(s, 1);
+            inputList.add(input);
+        }
+        //get the map for the next step of sizing the same word
+        Map<String, List<Input>> map = getListMap(inputList);
 
-    private Map<String,List<Input>> getListMap(List<Input> inputList) {
+        List<Input> frequencies = new ArrayList<>();
+        for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
+            Input input = new Input(entry.getKey(), entry.getValue().size());
+            frequencies.add(input);
+        }
+        return frequencies;
+    }
+
+    private Map<String, List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> map = new HashMap<>();
-        for (Input input :  inputList){
+        for (Input input : inputList) {
 //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
-            }
-
-            else {
+            if (!map.containsKey(input.getValue())) {
+                ArrayList words = new ArrayList<>();
+                words.add(input);
+                map.put(input.getValue(), words);
+            } else {
                 map.get(input.getValue()).add(input);
             }
         }
-
-
         return map;
     }
 
